66a2e3ecfb66f67b84ea4aca812a1bb8879bcf55
Alexandre Ferro <alexandreacff@gmail.com>
2024-07-09 17:39:18 -0300

    fix(miss_mic): fix timer and list_devices

--- ros/packages/miss_mic/miss_mic/mic.py
+++ ros/packages/miss_mic/miss_mic/mic.py
@@ -5,14 +5,13 @@ from std_msgs.msg import Int16MultiArray
 import pyaudio
 import numpy as np
 
-
 class MicrophonePublisher(Node):
     def __init__(self):
 
         super().__init__('microphone_publisher')
 
         self.publisher_ = self.create_publisher(Int16MultiArray, 'microphone_data', 10)
-        self.timer_ = self.create_timer(0.1, self.publish_microphone_data)
+        self.timer_ = self.create_timer(0.08, self.publish_microphone_data)
 
         # Specify the input device (example: device index 1)
         self.input_device = 8
@@ -23,14 +22,10 @@ class MicrophonePublisher(Node):
         rate = 16000
         self.chunk = 1280
         self.p = pyaudio.PyAudio()
+        self.list_devices()
+
         self.mic_stream = self.p.open(format=format, channels=channels, rate=rate, input=True, frames_per_buffer=self.chunk, input_device_index=self.input_device)
 
-        # Listar os dispositivos de entrada (microfones)
-        print("Dispositivos de entrada (microfones):")
-        for i in range(self.p.get_device_count()):
-            device_info = self.p.get_device_info_by_index(i)
-            if device_info["maxInputChannels"] > 0:
-              print(f"ID: {device_info['index']}, Nome: {device_info['name']}")
 
         self.get_logger().info(f"Recording ...")
 
@@ -44,6 +39,13 @@ class MicrophonePublisher(Node):
         audio_msg = Int16MultiArray()
         audio_msg.data = data.tolist()
         self.publisher_.publish(audio_msg)
+
+    def list_devices(self):
+        self.get_logger().info("Dispositivos de entrada (microfones):")
+        for i in range(self.p.get_device_count()):
+            device_info = self.p.get_device_info_by_index(i)
+            if device_info["maxInputChannels"] > 0:
+                self.get_logger().info(f"ID: {device_info['index']}, Nome: {device_info['name']}")
 
 
 def main(args=None):
