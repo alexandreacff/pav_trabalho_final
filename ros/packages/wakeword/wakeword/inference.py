551469ad47b69fb9e9b478a17e57e5c87909b9d5
Alexandre Ferro <alexandreacff@gmail.com>
2024-07-09 17:37:29 -0300

    feat(wakeword): add publisher and params

--- ros/packages/wakeword/wakeword/inference.py
+++ ros/packages/wakeword/wakeword/inference.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16MultiArray
+from std_msgs.msg import Int16MultiArray, Bool
 import numpy as np
 from openwakeword.model import Model
 from ament_index_python.packages import get_package_share_directory
@@ -10,8 +10,6 @@ import os
 class WakeWord(Node):
 
     def __init__(self):
-        self.data = None
-        self.data_diff = None
         super().__init__('wake_word_detector')
 
         self.subscription = self.create_subscription(
@@ -21,12 +19,17 @@ class WakeWord(Node):
             10
         )
 
-        # self.declare_parameter("engine", os.path.join(get_package_share_directory('yolo_pose'), 'config', 'yolov8n-pose.engine'))
-        # engine = self.get_parameter(
-        #     "engine").get_parameter_value().string_value
-        print(os.getcwd())
+        self.publisher = self.create_publisher(Bool, 'wakeword_status', 10)
 
-        self.model = Model(wakeword_models=['src/wakeword/config/models/hey_jarvis_v0.1.onnx'], inference_framework='onnx')
+        self.declare_parameter("model", 'hey_jarvis_v0.1.onnx')
+        model_path = os.path.join(get_package_share_directory('wakeword'), 'config', self.get_parameter(
+            "model").get_parameter_value().string_value)
+        self.get_logger().info(f"Model path: {model_path}")
+
+        self.status = Bool()
+        self.status.data = False
+
+        self.model = Model(wakeword_models=[model_path], inference_framework=os.path.splitext(model_path)[1][1:])
         self.mdl = list(self.model.models.keys())[0]
 
     def detector_callback(self, msg):
@@ -34,10 +37,12 @@ class WakeWord(Node):
 
         data =  np.array(msg.data)
         prediction = self.model.predict(data)
-        # scores = list(self.model.prediction_buffer[self.mdl])
-        # self.get_logger().info(f"{scores}")
+        scores = list(self.model.prediction_buffer[self.mdl])
         if scores[-1] > 0.8:
-            self.get_logger().info(f"Wakeword Detected! - Score: {scores[-1]}")
+            self.status.data = True
+        else:  
+            self.status.data = False
+        self.publisher.publish(self.status)
 
 
 
